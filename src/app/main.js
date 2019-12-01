930b6cab9894a28bfc13579d62bfcaf07f9a4b81
iigmir <roc120j@gmail.com>
2019-12-02 00:35:07 +0800

    ES6 class

--- src/app/main.js
+++ src/app/main.js
@@ -1,15 +1,17 @@
 import React from "react";
 import MoneyForm from "./Form.js";
 
-function App() {
-    return (
-        <div className="App">
-            <h1 className="ts center aligned header">Money app</h1>
-            <div className="ts container">
-                <MoneyForm />
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <h1 className="ts center aligned header">Money app</h1>
+                <div className="ts container">
+                    <MoneyForm />
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
